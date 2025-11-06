b88f7ed3d37c8590b309c33f5d2233d216d0e2f4
Julian Ray <68452898+Holorite@users.noreply.github.com>
2025-11-06 13:28:26 -0500

    Remove unnecessary files

--- scripts/tmux_get_notes.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# fetching the value of "tpm_plugins" option
-plugin_path=$(tmux show-env -g TMUX_PLUGIN_MANAGER_PATH | cut -f2 -d=)
-
-tmux display-popup ls -1 "$plugin_path"
